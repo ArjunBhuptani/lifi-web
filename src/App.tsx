1e6e09fd96422de2ee02dff199bda388d6050170
Philipp Zentner <philipp.zentner@stomt.com>
2021-09-28 23:43:17 +0200

    Added social links, FAQ, Developer Waitinglist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { WalletOutlined } from '@ant-design/icons';
-import { Button, Layout, Menu } from 'antd';
-import { Header } from 'antd/lib/layout/layout';
+import { WalletOutlined, TwitterOutlined, MediumOutlined, GithubOutlined } from '@ant-design/icons';
+import { Button, Layout, Menu, Row, Col } from 'antd';
+import { Header, Footer, Content } from 'antd/lib/layout/layout';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -44,96 +44,126 @@ function App() {
         <Web3ConnectionManager>
           <Layout>
             <Header style={{ position: 'fixed', zIndex: 900, width: '100%', padding: 0 }}>
-              <Link to="/" className="wordmark">
-                <img src={logo} className="logo" alt={process.env.REACT_APP_PROJECT_NAME} width="36" height="36" />
-                <span>Li.Finance</span>
-              </Link>
-              <Menu theme="light" mode="horizontal" defaultSelectedKeys={path ? [path] : []}>
-                <Menu.Item key="/dashboard">
-                  <Link to="/dashboard">Dashboard</Link>
-                </Menu.Item>
-                <Menu.Item key="/swap">
-                  <span className="beta-badge">Beta</span>
-                  <Link to="/swap">Swap</Link>
-                </Menu.Item>
-                <Menu.Item key="/about">
-                  <Link to="/about">About</Link>
-                </Menu.Item>
-                {false && <Menu.Item key="wallets" style={{ float: "right" }}>
-                  <Button shape="round" icon={<WalletOutlined />} >
-                    Add Wallets
-                  </Button>
-                </Menu.Item>}
-              </Menu>
-
-              <a className="lifiSupport" href="https://discord.com/invite/G9uAbE439B" target="_blank" rel="nofollow noreferrer">Support</a>
+              <Row>
+                <Col xs={2} sm={4} md={6} lg={10} xl={12}>
+                  <Link to="/" className="wordmark">
+                    <img src={logo} className="logo" alt={process.env.REACT_APP_PROJECT_NAME} width="36" height="36" />
+                    <span>Li.Finance</span>
+                  </Link>
+                  <Menu mode="horizontal" defaultSelectedKeys={path ? [path] : []}>
+                    <Menu.Item key="/dashboard">
+                      <Link to="/dashboard">Dashboard</Link>
+                    </Menu.Item>
+                    <Menu.Item key="/swap">
+                      <span className="beta-badge">Beta</span>
+                      <Link to="/swap">Swap</Link>
+                    </Menu.Item>
+                    <Menu.Item key="/about">
+                      <Link to="/about">About</Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a href="https://docs.li.finance/for-users/user-faq" target="_blank">FAQ</a>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSe4vZSN02dmN4W0V_-sB1Aw4erZh577L2h0aDbnzfoRhurPQQ/viewform?usp=send_form" target="_blank" rel="nofollow noreferrer">Developer Waitinglist</a>
+                    </Menu.Item>
+                    {false && <Menu.Item key="wallets" style={{ float: "right" }}>
+                      <Button shape="round" icon={<WalletOutlined />} >
+                        Add Wallets
+                      </Button>
+                    </Menu.Item>}
+                  </Menu>
+                </Col>
+                <Col xs={2} sm={4} md={6} lg={10} xl={12}>
+                  <Row justify="end">
+                    <Col>
+                      <a href="https://twitter.com/lifiprotocol" rel="nofollow">
+                        <TwitterOutlined style={{fontSize:'20px', color: '#ffffff'}}/>
+                      </a>
+                    </Col>
+                    <Col>
+                      <a href="https://github.com/lifinance" rel="nofollow" style={{marginLeft:'15px'}}>
+                        <GithubOutlined style={{fontSize:'20px', color: '#ffffff'}}  />
+                      </a>
+                    </Col>
+                    <Col>
+                      <a className="lifiSupport" href="https://discord.com/invite/G9uAbE439B" target="_blank" rel="nofollow noreferrer">Support (Discord)</a>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
             </Header>
 
-            <Switch>
-              <Redirect exact from="/" to="/swap" />
-              <Route path="/dashboard" render={() => {
-                setMetatags({
-                  title: 'Li.Finance - Dashboard',
-                })
-                initStomt('dashboard')
-                return <Dashboard />
-              }} />
-              <Route path="/swap" render={() => {
-                setMetatags({
-                  title: 'Li.Finance - Swap',
-                })
-                initStomt('swap')
-                const transferChains = [
-                  getChainByKey(ChainKey.POL),
-                  getChainByKey(ChainKey.BSC),
-                  getChainByKey(ChainKey.DAI),
-                  getChainByKey(ChainKey.FTM),
-                  getChainByKey(ChainKey.ETH),
-                  getChainByKey(ChainKey.ARB),
-                  getChainByKey(ChainKey.OPT),
-                ]
+            <Content>
+              <Switch>
+                <Redirect exact from="/" to="/swap" />
+                <Route path="/dashboard" render={() => {
+                  setMetatags({
+                    title: 'Li.Finance - Dashboard',
+                  })
+                  initStomt('dashboard')
+                  return <Dashboard />
+                }} />
+                <Route path="/swap" render={() => {
+                  setMetatags({
+                    title: 'Li.Finance - Swap',
+                  })
+                  initStomt('swap')
+                  const transferChains = [
+                    getChainByKey(ChainKey.POL),
+                    getChainByKey(ChainKey.BSC),
+                    getChainByKey(ChainKey.DAI),
+                    getChainByKey(ChainKey.FTM),
+                    getChainByKey(ChainKey.ETH),
+                    getChainByKey(ChainKey.ARB),
+                    getChainByKey(ChainKey.OPT),
+                  ]
 
-                return <div className="lifiWrap">
-                  <Swap
-                    transferChains={transferChains}
-                    getBalancesForWallet={getBalancesForWalletMainnet}
-                  />
-                </div>
-              }} />
-              <Route path="/testnet" render={() => {
-                setMetatags({
-                  title: 'Li.Finance - Testnet',
-                })
-                initStomt('swap')
-                const transferChains = [
-                  getChainByKey(ChainKey.RIN),
-                  getChainByKey(ChainKey.GOR),
-                  getChainByKey(ChainKey.ROP),
-                  getChainByKey(ChainKey.MUM),
-                ]
-                return <div className="lifiWrap">
-                  <Swap
-                    transferChains={transferChains}
-                    getBalancesForWallet={getBalancesForWalletTestnet}
-                  />
-                </div>
-              }} />
-              <Route path="/about" render={() => {
-                setMetatags({
-                  title: 'Li.Finance - About',
-                })
-                initStomt('lifi')
-                return <AboutPage />
-              }} />
-              <Route path="*" render={() => {
-                setMetatags({
-                  title: 'Li.Finance - Not Found',
-                  status: 404,
-                })
-                initStomt('lifi')
-                return <NotFoundPage />
-              }} />
-            </Switch>
+                  return <div className="lifiWrap">
+                    <Swap
+                      transferChains={transferChains}
+                      getBalancesForWallet={getBalancesForWalletMainnet}
+                    />
+                  </div>
+                }} />
+                <Route path="/testnet" render={() => {
+                  setMetatags({
+                    title: 'Li.Finance - Testnet',
+                  })
+                  initStomt('swap')
+                  const transferChains = [
+                    getChainByKey(ChainKey.RIN),
+                    getChainByKey(ChainKey.GOR),
+                    getChainByKey(ChainKey.ROP),
+                    getChainByKey(ChainKey.MUM),
+                  ]
+                  return <div className="lifiWrap">
+                    <Swap
+                      transferChains={transferChains}
+                      getBalancesForWallet={getBalancesForWalletTestnet}
+                    />
+                  </div>
+                }} />
+                <Route path="/about" render={() => {
+                  setMetatags({
+                    title: 'Li.Finance - About',
+                  })
+                  initStomt('lifi')
+                  return <AboutPage />
+                }} />
+                <Route path="*" render={() => {
+                  setMetatags({
+                    title: 'Li.Finance - Not Found',
+                    status: 404,
+                  })
+                  initStomt('lifi')
+                  return <NotFoundPage />
+                }} />
+              </Switch>
+            </Content>
+            <Footer>
+
+            </Footer>
           </Layout>
         </Web3ConnectionManager>
       </WrappedWeb3ReactProvider>
